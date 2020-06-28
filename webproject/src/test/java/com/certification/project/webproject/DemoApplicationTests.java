91458891a153cb34e948ec99646d81e091f9439c
LuisaFernanda <luisa.colorado.e@gmail.com>
2020-06-28 12:05:27 -0500

    Creación del proyecto en gradle

---
+++ webproject/src/test/java/com/certification/project/webproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certification.project.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
